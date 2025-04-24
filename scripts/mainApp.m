8e5bbaf1b101749a17d44f07b9968f20a8894899
Michal Trajdos <michal.trajdos95@gmail.com>
2025-04-24 16:07:24 +0200

    Instruction controller and display

--- scripts/mainApp.m
+++ scripts/mainApp.m
@@ -1,31 +1,83 @@
-% Set up PTB path
-addpath(genpath('C:\Users\michal\AppData\Roaming\MathWorks\MATLAB Add-Ons\Toolboxes\Psychtoolbox-3-3.0.19.15\'))
+%% Initialize PTB and its parameters
 
-% Initialize Psychtoolbox
-PsychDefaultSetup(2);
+    % Initialize Psychtoolbox
+    PsychDefaultSetup(1); 
+    
+    % Set parameters
+    screens = Screen('Screens');
+    screenNumber = max(screens);
+    
+    % Configure Screen preferences as in FFP2Direct
+    Screen('Preference', 'SkipSyncTests', 1);
+    Screen('Preference', 'ConserveVRAM', 64);
+    
+    % Define colors
+    white = WhiteIndex(screenNumber);
+    black = BlackIndex(screenNumber);
+    grey = white / 2;
+    midgray = round((white+black)/2);
+    inc = white-midgray;
+    
+    % Open window
+    [window, windowRect] = Screen('OpenWindow', screenNumber, grey);
+    
+    % Get timing parameters (from FFP2Direct)
+    FrameDurationInSeconds = Screen('GetFlipInterval', window);
+    Slack = FrameDurationInSeconds * 0.5;
+    
+    % Get screen dimensions
+    [xCenter, yCenter] = RectCenter(windowRect);
+    [screenXpixels, screenYpixels] = Screen('WindowSize', window);
+    
+    % Set text properties
+    if screenXpixels == 1024 && screenYpixels == 768
+        textSize = 18;
+    else
+        textSize = 24;
+    end
+    Screen('TextFont', window, 'Courier New');
+    Screen('TextSize', window, textSize);
+    Screen('TextStyle', window, 1+2);  % Bold + Italic
 
-% Set parameters
-screens = Screen('Screens');
-% screenNumber = max(screens);
-screenNumber = 1
+    % Hide cursor and suppress keypresses to Matlab window
+    HideCursor;
 
-% Configure Screen preferences as found in the FFP2Scenes script
-Screen('Preference', 'SkipSyncTests', 0);
-Screen('Preference', 'SyncTestSettings', 0.001, [], [], []);
-Screen('Preference', 'VisualDebugLevel', 0);
 
-% Get color definitions
-white = WhiteIndex(screenNumber);
+%% Initialize experiment parameters
 
-% Open window with specific parameters (color depth and buffer count)
-[window, windowRect] = Screen('OpenWindow', screenNumber, 0, [], 32, 2);
+lang = 'de';
 
-% Create instance
-app = aac();
+n_baselineTrials = 18;
+n_conflictTrials = 54;
 
-% Run phases
-app.displayInstruction(window, white, "baseline");
+angryFaces = dir(fullfile('../sprites/faces/angry', '*.jpg'));
+neutralFaces = dir(fullfile('../sprites/faces/neutral', '*.jpg'));
+decisionWindow = double(4.000000);
 
-% Clean up
-WaitSecs(2);
-Screen('CloseAll');
+aversiveProbability = int32(0);
+score = int32(0);
+
+% Run the application
+
+try
+    % Create module instances
+    ic = instructionController();
+
+    % Display initial instruction
+    ic.displayInstruction(window, white, lang, 0)
+    
+    % Wait for a key press before continuing
+    KbStrokeWait;
+
+    % Clean up
+    Screen('CloseAll');
+    ShowCursor;
+    
+catch error
+    % Clean up in case of error
+    Screen('CloseAll');
+    ShowCursor;
+    
+    % Display error information
+    rethrow(error);
+end
---
+++ scripts/trialController.m
@@ -0,0 +1,11 @@
+classdef trialController
+    properties
+        % Initialize parameters
+    end
+    
+    methods
+        function placeholderFunction(obj, window, white, phase)
+            end
+        end
+    end
+end
