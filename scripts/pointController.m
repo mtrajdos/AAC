7a5464670f80648c414e3ea861bcd03cb9a65711
Michal Trajdos <michal.trajdos95@gmail.com>
2025-05-05 14:39:41 +0200

    Initial expansion for conflict trials

---
+++ scripts/pointController.m
@@ -0,0 +1,12 @@
+classdef pointController
+    properties
+        lowRewardCount = int32();
+        midRewardCount = int32();
+        highRewardCount = int32();
+    end
+    methods
+        function reward = drawReward()
+
+        end
+    end
+end
